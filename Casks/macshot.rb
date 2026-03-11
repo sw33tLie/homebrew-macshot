45703c8de9f35d1025c79f0cbe094875a563a39b
sw33tLie <sw33tlie@protonmail.com>
2026-03-11 21:28:09 +0000

    Update macshot to v1.2.0

--- Casks/macshot.rb
+++ Casks/macshot.rb
@@ -1,6 +1,6 @@
 cask "macshot" do
-  version "1.1.1"
-  sha256 "eedf11dbc97700224fe8b21634fb06f240b783da837b90b259b56c68cc09859e"
+  version "1.2.0"
+  sha256 "e591a90bc11e0cacc03d5dd2b10ef4318b419b4426d372234d4a4f3007a94f1b"
 
   url "https://github.com/sw33tLie/macshot/releases/download/v#{version}/macshot.zip"
   name "macshot"
